486d2c6c366cbce579328a607a37934ba886826e
Simone Serpi <simoneserpi@hotmail.it>
2022-03-31 16:04:19 +0200

    Add project files.

---
+++ hello-csharp/hello-csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Benvenuto!!");
+Console.WriteLine("Gennaio!");
+Console.WriteLine("Febbraio");
+Console.WriteLine("Marzo");
+Console.WriteLine("Aprile");
+Console.WriteLine("Maggio");
+Console.WriteLine("Giugno");
+Console.WriteLine("Luglio");
+Console.WriteLine("Agosto");
+Console.WriteLine("Settembre");
+Console.WriteLine("Ottobre");
+Console.WriteLine("Novembre");
+Console.WriteLine("Dicembre");
+
+Console.WriteLine("Qual' è il tuo mese preferito?");
+var mese = Console.ReadLine();
+
+Console.WriteLine("il tuo mese preferito è");
+Console.WriteLine(mese);
